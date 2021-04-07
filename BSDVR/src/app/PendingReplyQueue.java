3414a6c0ca52b681d110c7059f6004a5399dce35
ammar086 <farooq@knights.ucf.edu>
2021-04-07 14:38:56 -0700

    added event class to remove redundancy in pending reply queue

--- BSDVR/src/app/PendingReplyQueue.java
+++ BSDVR/src/app/PendingReplyQueue.java
@@ -6,64 +6,64 @@ import java.util.ArrayList;
 import java.nio.ByteBuffer;
 import java.io.OutputStream;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class PendingReplyQueue implements Runnable{
+    static class Entry{
+        private Long timestamp;
+        private Integer neighbor;
+        private ArrayList<Integer> destinations;
+        //constructor
+        public Entry(Long ts, Integer n, ArrayList<Integer> dests){
+            timestamp = ts;
+            neighbor = n;
+            destinations = new ArrayList<Integer>();
+            for(Integer d:dests)destinations.add(d);
+        }
+        //getters
+        public Long getTimeStamp(){return timestamp;}
+        public Integer getNeighbor(){return neighbor;}
+        public ArrayList<Integer> getDestinations(){return destinations;}
+    }
     //elements
     private Router router;
     private Integer curr_neighbor;
     private ArrayList<Integer> curr_destinations;
-    private ConcurrentLinkedQueue<Long> reply_timers;
-    private ConcurrentHashMap<Long, Integer> reply_neighbors;
-    private ConcurrentHashMap<Long,ArrayList<Integer>> reply_destinations;
+    private ConcurrentLinkedQueue<Entry> reply_queue;
     //constructor    
     public PendingReplyQueue(Router r){
         router = r;
-        reply_timers = new ConcurrentLinkedQueue<Long>();
-        reply_neighbors = new ConcurrentHashMap<Long, Integer>();
-        reply_destinations = new ConcurrentHashMap<Long, ArrayList<Integer>>();
+        reply_queue = new ConcurrentLinkedQueue<Entry>();
     }
     //methods
     public synchronized void removeReply(){
-        // System.out.println("removeReply()");
-        Iterator<Long> iter;
-        Long tmp, curr_time, timeDiff, timeElapsed; 
+        Entry tmp;
+        Iterator<Entry> iter;
+        Long curr_time, timeDiff, timeElapsed; 
         // initializing values
         curr_neighbor = -1;
-        iter = reply_timers.iterator();
+        iter = reply_queue.iterator();
         curr_time = Instant.now().toEpochMilli();
         curr_destinations = new ArrayList<Integer>();
         while(iter.hasNext()){
             tmp = iter.next();
-            timeDiff = curr_time - tmp;
+            timeDiff = curr_time - tmp.getTimeStamp();
             timeElapsed = TimeUnit.MILLISECONDS.toMillis(timeDiff);
             if(timeElapsed > Constants.TIMER_BASE){
-                curr_neighbor = reply_neighbors.get(tmp);
+                curr_neighbor = tmp.getNeighbor();
                 curr_destinations = new ArrayList<Integer>();
-                for(Integer dest:reply_destinations.get(tmp)){curr_destinations.add(dest);}
-                // System.out.println("Removing");
-                // System.out.print(Router.translateID(curr_neighbor) + " " + reply_timers.size() + " " + reply_neighbors.size() + " " + reply_destinations.size() + " [");
-                // for(Integer d:curr_destinations)System.out.print(" " + d);
-                // System.out.println(" ]");
-                // System.out.println(reply_timers.size() + " " + reply_neighbors.size() + " " + reply_destinations.size());
-                reply_timers.remove(tmp);
-                reply_neighbors.remove(tmp);
-                reply_destinations.remove(tmp);
-                iter = reply_timers.iterator();
+                for(Integer dest:tmp.getDestinations()){curr_destinations.add(dest);}
+                reply_queue.remove(tmp);
+                iter = reply_queue.iterator();
                 break;
             }
         }
     }
     public synchronized void insertReply(Long timestamp, Integer neighbor, ArrayList<Integer> destinations){
         if(destinations.size() > 0){
-            // System.out.println("\ninsertReply()");
-            reply_timers.add(timestamp);
-            reply_neighbors.put(timestamp, neighbor);
-            reply_destinations.put(timestamp, destinations);
-            // System.out.print(Router.translateID(neighbor) + " " + reply_timers.size() + " " + reply_neighbors.size() + " " + reply_destinations.size() + " [");
-            // for(Integer d:destinations)System.out.print(" " + d);
-            // System.out.println(" ]");
+            Entry new_entry;
+            new_entry = new Entry(timestamp, neighbor, destinations);
+            reply_queue.add(new_entry);
         }
     }
     @Override
@@ -74,16 +74,12 @@ public class PendingReplyQueue implements Runnable{
         ArrayList<Integer> final_vec_rt;
         Integer next_hop, curr_dest_state, curr_dest_cost_c1;
         while(router.getRFlag()){
-            // System.out.println("HERE");
             try {
-                if(reply_destinations.size() > 0  && reply_timers.size() > 0 && reply_neighbors.size() > 0){
+                if(reply_queue.size() > 0){
                     removeReply();
                     final_vec_rt = new ArrayList<Integer>();
                     if(curr_destinations.size() > 0 && router.getLT().get(curr_neighbor).getLinkState() == 1){
                         out = router.getLT().get(curr_neighbor).getWrite(curr_neighbor);
-                        // System.out.print("[ ");
-                        // for(Integer d:curr_destinations)System.out.print(" " + d);
-                        // System.out.println(" ]");
                         // seperate active entries avaialble
                         for(Integer dest:curr_destinations){
                             if(router.getFT().containsKey(dest)){
@@ -92,10 +88,6 @@ public class PendingReplyQueue implements Runnable{
                                 if(curr_dest_state == 1){final_vec_rt.add(dest);}
                             }
                         }
-                        // System.out.println("Available");
-                        // System.out.print(Router.translateID(curr_neighbor) + " " + reply_timers.size() + " " + reply_neighbors.size() + " " + reply_destinations.size() + " [");
-                        // for(Integer d:final_vec_rt)System.out.print(" " + d);
-                        // System.out.println(" ]");
                         // send reply for pending active entries available
                         vec_available = ByteBuffer.allocate(final_vec_rt.size() * 12);
                         for(Integer dest:final_vec_rt){
@@ -113,9 +105,9 @@ public class PendingReplyQueue implements Runnable{
                         }
                     }
                 }
+                TimeUnit.MILLISECONDS.sleep(Constants.TIMER_BASE / 10);
             } catch (Exception e) {}
         }
-        // System.out.println(router.getID()+ ": HERE-BAD");
     }   
 }
 
